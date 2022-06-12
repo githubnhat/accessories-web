95b4b87d4a0b044fe6b8bf5e33f9b6f0392fe44b
haokyln <haokyln@gmail.com>
2022-06-12 10:04:24 +0700

    Feat: form add attribute for product

---
+++ frontend/src/services/admin/index.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+// axios.defaults.baseURL = 'http://localhost:1010/';
+
+const token = localStorage.getItem('accessToken');
+axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
+
+type ProductAttribute = {
+  id: number;
+  attributeName: string;
+};
+
+type GetProductAttributeResponse = {
+  data: Array<ProductAttribute>;
+};
+
+export async function getProductAttributes(path: string) {
+  try {
+    const { data, status } = await axios.get<GetProductAttributeResponse>(path);
+    console.log(JSON.stringify(data, null, 10));
+    // console.log('axios', data);
+    // 👇️ "response status is: 200"
+    console.log('response status is: ', status);
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+export default {};
