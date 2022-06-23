96509b84e160e748534d8319b570e1ec8cc11dc1
haokyln <haokyln@gmail.com>
2022-06-23 22:56:35 +0700

    Feat: register service

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,6 +1,8 @@
 import router from '@/router';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:8081/api/v1';
+
 type User = {
   accessToken: string;
 };
@@ -9,12 +11,42 @@ type GetUsersResponse = {
   data: User;
 };
 
-export async function doLogin(path: string, dataForm: object) {
+// login into system
+
+export async function doLogin(dataForm: object) {
   try {
+    const path = '/login';
     const { data, status } = await axios.post<GetUsersResponse>(path, dataForm);
     // console.log(JSON.stringify(data, null, 10));
     localStorage.setItem('accessToken', data?.data?.accessToken || '');
     router.push('/');
+    // 👇️ "response status is: 200"
+    console.log('response status is: ', status);
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+// register new account
+
+export async function doRegister(dataForm: object) {
+  try {
+    const path = '/register';
+
+    const { data, status } = await axios.post<GetUsersResponse>(path, dataForm);
+    // console.log(JSON.stringify(data, null, 10));
+    localStorage.setItem('accessToken', data?.data?.accessToken || '');
+    if (status === 200) {
+      router.push('/');
+    }
     // 👇️ "response status is: 200"
     console.log('response status is: ', status);
 
