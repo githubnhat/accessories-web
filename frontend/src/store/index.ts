250302c5d38e4f2513cb3c8a87866242acfa0cce
haokyln <haokyln@gmail.com>
2022-07-14 14:32:59 +0700

    Feat: presistent state

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import _accessToken from './_accessToken';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    _accessToken,
+  },
 });
