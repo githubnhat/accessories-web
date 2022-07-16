f2dbde6cfe06529a66799aa526a83360d5737421
githubnhat <18130166@st.hcmuaf.edu.vn>
2022-07-16 12:32:18 +0700

    upload img

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,8 +6,23 @@ import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 import '@/plugins/vee';
-import {setup} from '@/interceptors/axios-setup'
+import {setup} from '@/interceptors/axios-setup';
+import { initializeApp } from 'firebase/app';
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCihS1CDNxkc3z-y60KJECdHJdfhe9XvpY",
+  authDomain: "minhnhat569-eecaa.firebaseapp.com",
+  databaseURL: "https://minhnhat569-eecaa.firebaseio.com",
+  projectId: "minhnhat569-eecaa",
+  storageBucket: "minhnhat569-eecaa.appspot.com",
+  messagingSenderId: "987914771144",
+  appId: "1:987914771144:web:ad674ef460d78d4753a3fd",
+  measurementId: "G-LMZ881S20T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 setup();
 new Vue({
   router,
--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -1,12 +1,53 @@
 <template>
-  <auth-provider> <div>This is profile page</div> </auth-provider>
+  <auth-provider>
+    <div>This is profile page</div>
+    <v-form @submit.prevent="uploadFile">
+      <v-col cols="12">
+        <v-file-input
+          v-model="files"
+          placeholder="Thêm ảnh"
+          label="Ảnh sản phẩm"
+          multiple
+          outlined
+          prepend-icon="mdi-image"
+          dense
+        >
+          <template v-slot:selection="{ text }">
+            <v-chip small label color="primary">
+              {{ text }}
+            </v-chip>
+          </template></v-file-input
+        >
+      </v-col>
+      <div class="d-flex justify-end">
+        <v-btn color="primary" type="submit"> Tiếp tục </v-btn>
+      </div>
+    </v-form>
+    <div v-if="images.length > 0">
+      <div v-for="img in images" :key="img.src"><img :src="img.src" alt="" /></div>
+    </div>
+  </auth-provider>
 </template>
 
 <script>
 import AuthProvider from '@/auth/AuthProvider.vue';
+import { upLoadImg } from '@/services/uploadFile';
 export default {
   name: 'ProfilePage',
+  data() {
+    return {
+      files: [],
+      images: [],
+    };
+  },
+
   components: { AuthProvider },
+  methods: {
+    async uploadFile() {
+      this.images = await upLoadImg(this.files);
+      this.files = null;
+    },
+  },
 };
 </script>
 
