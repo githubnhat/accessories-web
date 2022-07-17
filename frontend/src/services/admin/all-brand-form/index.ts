4de5150229acc71bec0d1db0245479462de249eb
haokyln <haokyln@gmail.com>
2022-07-17 15:34:09 +0700

    Feat: add service for all brand form

---
+++ frontend/src/services/admin/all-brand-form/index.ts
@@ -0,0 +1,117 @@
+import axios from 'axios';
+
+type Brand = {
+  id: number;
+  name: string;
+  code: string;
+};
+
+type BrandResponse = {
+  page: number;
+  totalPages: number;
+  totalItems: number;
+  data: Array<Brand>;
+};
+
+type GetBrandResponse = {
+  data: BrandResponse;
+};
+export async function getAllBrand(pageNumber: number, itemsPerPage: number) {
+  try {
+    const { data, status } = await axios.get<GetBrandResponse>(
+      `admin/brand/page/${pageNumber}/limit/${itemsPerPage}`,
+      {
+        withCredentials: true,
+      },
+    );
+    console.log('response status is: ', status);
+    return data?.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+export async function insertBrand(dataForm: object) {
+  try {
+    const path = 'admin/brand';
+    const { data, status } = await axios.post<GetBrandResponse>(path, dataForm, {
+      withCredentials: true,
+    });
+    // console.log(JSON.stringify(data, null, 10));
+
+    console.log('response status is: ', status);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+// check unique brand name
+
+type CheckUniqueBrandNameResponse = {
+  data: boolean;
+};
+
+export async function checkUniqueBrandName(name: string) {
+  try {
+    const path = `admin/brand/exists/${name}`;
+    const { data, status } = await axios.get<CheckUniqueBrandNameResponse>(path, {
+      withCredentials: true,
+    });
+    // console.log(JSON.stringify(data, null, 10));
+
+    // 👇️ "response status is: 200"
+    console.log('response status is: ', status);
+
+    return data?.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+// check unique brand code
+
+type CheckUniqueBrandCodeResponse = {
+  data: boolean;
+};
+
+export async function checkUniqueBrandCode(code: string) {
+  try {
+    const path = `admin/brand/existsCode/${code}`;
+    const { data, status } = await axios.get<CheckUniqueBrandCodeResponse>(path, {
+      withCredentials: true,
+    });
+    // console.log(JSON.stringify(data, null, 10));
+
+    // 👇️ "response status is: 200"
+    console.log('response status is: ', status);
+
+    return data?.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
