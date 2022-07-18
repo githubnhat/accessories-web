1c63a964466b40f45dd318e94959e7bb2e5321f4
githubnhat <18130166@st.hcmuaf.edu.vn>
2022-07-18 13:17:35 +0700

    update/delete Attr

--- frontend/src/components/Forms/Admin/MAllAttributeForm.vue
+++ frontend/src/components/Forms/Admin/MAllAttributeForm.vue
@@ -2,79 +2,235 @@
   <div>
     <div class="display-1 text-center mb-3">Danh sách thuộc tính</div>
     <v-data-table
+      v-model="selectedItems"
       :page="page"
       :pageCount="totalPages"
       :headers="headers"
       :items="data"
       :options.sync="options"
       :server-items-length="totalItems"
-      :loading="loading"
+      :loading="loading.table"
+      class="elevation-1"
       :footer-props="{
         showFirstLastPage: true,
         showCurrentPage: true,
       }"
-      class="elevation-1"
+      show-select
     >
       <template v-slot:top>
         <v-toolbar flat>
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="500px">
+          <v-dialog v-model="dialog.deleteAll" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                :disabled="selectedItems.length === 0"
+                color="primary"
+                dark
+                class="mb-2 mr-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                Xóa
+              </v-btn>
+            </template>
+            <v-card>
+              <ValidationObserver v-slot="{ handleSubmit }">
+                <v-form @submit.prevent="handleSubmit(deleteAttributes)">
+                  <v-card-title>
+                    <span class="text-h5">Xác nhận xóa (các) danh mục sau:</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-row dense v-for="item in selectedItems" :key="item.id">
+                      <v-col cols="6">
+                        <v-text-field
+                          v-model="item.attributeName"
+                          label="Tên thuộc tính"
+                          outlined
+                          dense
+                          readonly
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="6">
+                        <v-text-field
+                          v-model="item.variantNames"
+                          label="Giá trị"
+                          outlined
+                          dense
+                          readonly
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDelete"> Trở lại </v-btn>
+                    <v-btn
+                      color="error darken-1"
+                      text
+                      type="submit"
+                      :loading="loading.deleteListAttribute"
+                    >
+                      Xóa
+                    </v-btn>
+                  </v-card-actions>
+                </v-form>
+              </ValidationObserver>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialog.add" max-width="500px">
             <template v-slot:activator="{ on, attrs }">
               <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
                 Thêm thuộc tính
               </v-btn>
             </template>
             <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
+              <ValidationObserver v-slot="{ handleSubmit }">
+                <v-form @submit.prevent="handleSubmit(addNewAttributes)">
+                  <v-card-title>
+                    <span class="text-h5">Thêm danh mục</span>
+                  </v-card-title>
 
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.attributeName"
-                        label="Tên thuộc tính"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="12">
-                      <v-text-field
-                        v-model="editedItem.variantNames"
-                        label="Các giá trị được cách nhau bằng dấu phẩy (,)"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
+                  <v-card-text>
+                    <v-row>
+                      <v-col cols="12">
+                        <validation-provider
+                          name="Tên danh mục"
+                          rules="required|uniqueAttributeName"
+                          v-slot="{ errors }"
+                        >
+                          <v-text-field
+                            v-model="editedItem.attributeName"
+                            label="Tên thuộc tính"
+                            :error-messages="errors"
+                            outlined
+                            dense
+                          ></v-text-field>
+                        </validation-provider>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="editedItem.variantNames"
+                          label="Các giá trị được cách nhau bằng dấu phẩy (,)"
+                          :error-messages="errors"
+                          outlined
+                          dense
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close"> Trở lại </v-btn>
-                <v-btn color="blue darken-1" text v-if="formAction === 'save'" @click="save">
-                  Lưu
-                </v-btn>
-                <v-btn color="blue darken-1" text v-else @click="update"> Cập nhật </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">Bạn có chắc chắn xoá thuộc tính?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Trở lại</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">Đồng ý</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeAdd"> Trở lại </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      type="submit"
+                      :loading="loading.addNewAttributeButton"
+                    >
+                      Thêm mới
+                    </v-btn>
+                  </v-card-actions>
+                </v-form>
+              </ValidationObserver>
             </v-card>
           </v-dialog>
         </v-toolbar>
       </template>
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      <template v-slot:[`item.actions`]>
+        <!-- <template v-slot:[`item.actions`]="{ item }"> -->
+        <!-- <v-dialog v-model="dialog.edit" max-width="500px">
+          <v-card>
+            <ValidationObserver v-slot="{ handleSubmit }">
+              <v-form class="mt-5" @submit.prevent="handleSubmit(editItemConfirm(item))">
+                <v-card-title class="text-h5">Chỉnh sửa thông tin thuộc tính</v-card-title>
+                <v-card-text>
+                  <v-row no-gutters dense>
+                    <v-col cols="12">
+                      <validation-provider
+                        name="Tên thuộc tính"
+                        rules="required|uniqueAttributeName"
+                        v-slot="{ errors }"
+                      >
+                        <v-text-field
+                          v-model="item.attributeName"
+                          label="Tên thuộc tính"
+                          prepend-icon="mdi-order-alphabetical-ascending"
+                          color="primary accent-3"
+                          clearable
+                          :error-messages="errors"
+                        >
+                        </v-text-field>
+                      </validation-provider>
+                      <v-text-field
+                        v-model="item.variantNames"
+                        label="Các giá trị được cách nhau bằng dấu phẩy (,)"
+                        prepend-icon="mdi-alphabetical"
+                        color="primary accent-3"
+                        clearable
+                        :error-messages="errors"
+                      >
+                      </v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeEdit">Trở lại</v-btn>
+                  <v-btn color="blue darken-1" text type="submit">Đồng ý</v-btn>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-form>
+            </ValidationObserver>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialog.delete" max-width="500px">
+          <v-card>
+            <ValidationObserver v-slot="{ handleSubmit }">
+              <v-form @submit.prevent="handleSubmit(deleteOneItem(item))">
+                <v-card-title>
+                  <span class="text-h5">Xác nhận xóa (các) danh mục sau:</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-row dense>
+                    <v-col cols="6">
+                      <v-text-field
+                        v-model="item.attributeName"
+                        label="Tên thuộc tính"
+                        outlined
+                        dense
+                        readonly
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field
+                        v-model="item.variantNames"
+                        label="Giá trị"
+                        outlined
+                        dense
+                        readonly
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete"> Trở lại </v-btn>
+                  <v-btn color="error darken-1" text type="submit" :loading="loading.delete">
+                    Xóa
+                  </v-btn>
+                </v-card-actions>
+              </v-form>
+            </ValidationObserver>
+          </v-card>
+        </v-dialog> -->
+        <v-icon small class="mr-2" @click="openEditDialog"> mdi-pencil </v-icon>
+        <v-icon small @click="openDeleteDialog"> mdi-delete </v-icon>
       </template>
       <template v-slot:no-data>
         <v-btn color="primary" @click="initialize"> Reset </v-btn>
@@ -83,31 +239,51 @@
   </div>
 </template>
 <script>
-import { getAllAttributes, addNewAttributes } from '@/services/admin/attribute/index';
+import {
+  getAllAttributes,
+  addNewAttributes,
+  deleteAttributes,
+} from '@/services/admin/attribute/index';
 export default {
   data: () => ({
     page: 0,
     totalItems: 0,
     totalPages: 0,
     itemsPerPage: 0,
-    loading: true,
     options: {
       page: 1,
       itemsPerPage: 10,
     },
-    dialog: false,
-    dialogDelete: false,
+    loading: {
+      table: true,
+      addNewAttributeButton: false,
+      deleteListAttribute: false,
+      delete: false,
+    },
+    dialog: {
+      add: false,
+      delete: false,
+      deleteAll: false,
+      edit: false,
+    },
+    // dialog: false,
+    // dialogDelete: false,
     search: '',
     headers: [
       {
-        text: 'Tên thuộc tính',
+        text: 'ID',
         align: 'start',
+        value: 'id',
+      },
+      {
+        text: 'Tên thuộc tính',
         value: 'attributeName',
       },
       { text: 'Giá trị', value: 'variantNames' },
       { text: 'Hành động', value: 'actions', sortable: false },
     ],
     data: [],
+    selectedItems: [],
     editedIndex: -1,
     editedItem: {
       attributeName: '',
@@ -119,22 +295,9 @@ export default {
     },
   }),
 
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? 'Thêm thuộc tính' : 'Chỉnh sửa thuộc tính';
-    },
-    formAction() {
-      return this.editedIndex === -1 ? 'save' : 'update';
-    },
-  },
+  computed: {},
 
   watch: {
-    dialog(val) {
-      val || this.close();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
-    },
     options: {
       handler() {
         this.readDataFromAPI();
@@ -145,7 +308,7 @@ export default {
 
   methods: {
     async readDataFromAPI() {
-      this.loading = true;
+      this.loading.table = true;
       const { page, itemsPerPage } = this.options;
       this.data = await getAllAttributes(page, itemsPerPage);
       //Then injecting the result to datatable parameters.
@@ -153,19 +316,7 @@ export default {
       this.totalItems = this.data.totalItems;
       this.totalPages = this.data.totalPages;
       this.data = this.data.data;
-      this.loading = false;
-    },
-
-    editItem(item) {
-      this.editedIndex = this.data.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteItem(item) {
-      this.editedIndex = this.data.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
+      this.loading.table = false;
     },
 
     deleteItemConfirm() {
@@ -173,23 +324,61 @@ export default {
       this.closeDelete();
     },
 
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+    openEditDialog() {
+      this.dialog.edit = true;
+    },
+
+    openDeleteDialog() {
+      this.dialog.delete = true;
+    },
+    openDeleteAllDialog() {
+      this.dialog.deleteAll = true;
+    },
+
+    closeAdd() {
+      this.dialog.add = false;
     },
 
     closeDelete() {
-      this.dialogDelete = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+      // this.dialog.deleteAll = false;
+      this.dialog.delete = false;
     },
 
-    async save() {
+    async deleteOneItem(item) {
+      console.log(item);
+      this.selectedItems.push(item);
+      this.loading.delete = true;
+      const chosenItems = this.selectedItems.map((item) => item.id);
+      this.dialog.delete = false;
+      this.selectedItems = [];
+      console.log('chosenItems', chosenItems);
+      const data = await deleteAttributes(chosenItems);
+      if (data === true) {
+        this.readDataFromAPI();
+        alert('Xoá thành công');
+      } else {
+        alert('Xoá không thành công, vui lòng thử lại!');
+      }
+      this.loading.delete = false;
+    },
+
+    async deleteAttributes() {
+      this.loading.deleteAttributes = true;
+      const chosenItems = this.selectedItems.map((item) => item.id);
+      this.dialog.deleteAll = false;
+      this.selectedItems = [];
+      console.log('chosenItems', chosenItems);
+      const data = await deleteAttributes(chosenItems);
+      if (data === true) {
+        this.readDataFromAPI();
+        alert('Xoá thành công');
+      } else {
+        alert('Xoá không thành công, vui lòng thử lại!');
+      }
+      this.loading.deleteAttributes = false;
+    },
+
+    async addNewAttributes() {
       var listVariantNames = this.editedItem.variantNames.split(',').map((a) => a.trim());
       var res = [];
       listVariantNames.forEach(function (i) {
@@ -212,7 +401,7 @@ export default {
       };
       const newData = await addNewAttributes(request);
       if (newData !== null) this.readDataFromAPI();
-      this.close();
+      this.closeAdd();
     },
 
     async update() {
