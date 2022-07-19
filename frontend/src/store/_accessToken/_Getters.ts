e0967fe44180b5452682de3be61212521e003d0c
haokyln <haokyln@gmail.com>
2022-07-19 13:54:34 +0700

    Feat: adding getter for accesstoken in store

--- frontend/src/store/_accessToken/_Getters.ts
+++ frontend/src/store/_accessToken/_Getters.ts
@@ -1,2 +1,9 @@
-const getters = {};
+interface AccessToken {
+  accessToken: string;
+}
+const getters = {
+  getAccessToken(module: any): AccessToken {
+    return module.state.accessToken;
+  },
+};
 export default getters;
