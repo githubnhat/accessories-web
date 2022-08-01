ec2b85411036c30c9a35215064997cea747a3758
haokyln <haokyln@gmail.com>
2022-08-02 02:45:34 +0700

    Fix: line footer

--- frontend/src/pages/HomeView.vue
+++ frontend/src/pages/HomeView.vue
@@ -2,7 +2,7 @@
   <m-layout>
     <m-slide-banners></m-slide-banners>
     <v-divider></v-divider>
-    <v-card flat width="100%" class="d-flex flex-wrap mb-10 px-16" color="blue-grey lighten-5">
+    <v-card flat width="100%" class="d-flex flex-wrap mb-16 px-16" color="blue-grey lighten-5">
       <v-col cols="2" v-for="(item, index) in products" :key="index">
         <product-item :item="item"></product-item>
       </v-col>
